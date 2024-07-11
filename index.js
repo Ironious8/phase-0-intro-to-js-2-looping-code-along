ef58d7674d80502fa6c50d80781b9c5ea3f53581
Ironious8 <tyronekariuki@gmail.com>
2024-07-11 11:49:38 +0300

    Add loops

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
-// Code your solutions in this file
+function writeCards(names, surprise) {
+    let messages=[];
+    for(let i=0; i < names.length; i++ ) {
+    messages.push(`Thank you, ${names[i]}, for the wonderful ${surprise} gift!`)
+    }
+ return messages;
+}
+
+
+function countDown(number=10) {
+    while(number >= 0) {
+      console.log(number);
+      number--;
+
+
+    }
+}
+
+
